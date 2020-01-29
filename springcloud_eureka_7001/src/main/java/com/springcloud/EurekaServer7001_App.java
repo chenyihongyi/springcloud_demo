99458eed3d08ca3931bb99cc04d15d72037c8e73
Your Name <chenyihongyi@163.com>
2020-01-30 03:22:36 +0800

    提交代码

--- springcloud_eureka_7001/src/main/java/com/springcloud/EurekaServer7001_App.java
+++ springcloud_eureka_7001/src/main/java/com/springcloud/EurekaServer7001_App.java
@@ -2,17 +2,17 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author: Elvis
  * @Description:
- * @Date: 2020/1/28 5:09
+ * @Date: 2020/1/29 4:17
  */
 @SpringBootApplication
-@EnableEurekaClient //本服务启动后自动注册进eureka
-public class DeptProvider8001_App {
+@EnableEurekaServer
+public class EurekaServer7001_App {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class, args);
+        SpringApplication.run(EurekaServer7001_App.class, args);
     }
 }
