c0fe40c8dee199e0da2f189941e4ec122bb2951b
Your Name <chenyihongyi@163.com>
2020-01-30 04:46:33 +0800

    提交代码

--- springcloud_provider_dept_8001/src/main/java/com/springcloud/controller/DeptController.java
+++ springcloud_provider_dept_8001/src/main/java/com/springcloud/controller/DeptController.java
@@ -3,6 +3,9 @@ package com.springcloud.controller;
 import com.springcloud.demo.entity.Dept;
 import com.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +21,9 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
+    @Autowired
+    private DiscoveryClient client;
+
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
         return deptService.add(dept);
@@ -32,4 +38,19 @@ public class DeptController {
     public List<Dept> list() {
         return deptService.list();
     }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("SPRINGCLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
+
 }
